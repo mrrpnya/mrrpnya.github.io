24d3070eefdf4d9655cd97052a83a10b5eef983f
Seven Of Aces <sevenofaces42@gmail.com>
2025-01-25 20:10:20 -0800

    added venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/aces/Documents/blog/utils/pageupdater/venv"
+set -gx VIRTUAL_ENV "/home/aces/Documents/blog/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
